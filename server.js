08b92ed632b972b570b79caf682b04c69318b017
Abdurahmanit <231429@astanait.edu.kz>
2025-02-02 20:04:28 +0600

    Added /about route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! Deployed with Heroku');
+    res.send('Hello, World! Deployed with Render');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is the About page');
 });
 
 const PORT = process.env.PORT || 3000;
